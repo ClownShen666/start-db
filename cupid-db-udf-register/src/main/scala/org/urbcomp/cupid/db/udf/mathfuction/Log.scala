429c238f7c395d87b8fe8ec1cf4a4c88a67c3989
TYXXXXXXX <2237952894@qq.com>
2023-04-04 17:00:33 +0800

    暂存

--- cupid-db-udf-register/src/main/scala/org/urbcomp/cupid/db/udf/mathfuction/Log.scala
+++ cupid-db-udf-register/src/main/scala/org/urbcomp/cupid/db/udf/mathfuction/Log.scala
@@ -1,3 +1,19 @@
+/* 
+ * Copyright (C) 2022  ST-Lab
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.urbcomp.cupid.db.udf.mathfuction
 
 import org.urbcomp.cupid.db.udf.{AbstractUdf, DataEngine}
@@ -16,16 +32,18 @@ class Log extends Serializable with AbstractUdf {
   override def udfSparkEntryName(): String = "udfWrapper"
 
   /**
-   * Returns the (base) logarithm of a double value (num).
-   *
-   * @param base double
-   * @param num  double
-   * @return log result
-   */
+    * Returns the (base) logarithm of a double value (num).
+    *
+    * @param base double
+    * @param num  double
+    * @return log result
+    */
   def udfImpl(base: BigDecimal, num: BigDecimal): BigDecimal = {
-    if (base == null || num == null || base.doubleValue <= 0 || base.doubleValue == 1 || num.doubleValue == 0) return null
+    if (base == null || num == null || base.doubleValue <= 0 || base.doubleValue == 1 || num.doubleValue == 0)
+      return null
     val res = Math.log(num.doubleValue) / Math.log(base.doubleValue)
-     BigDecimal.valueOf(res)  }
+    BigDecimal.valueOf(res)
+  }
 
-  def udfWrapper: (BigDecimal,BigDecimal) => BigDecimal = udfImpl
+  def udfWrapper: (BigDecimal, BigDecimal) => BigDecimal = udfImpl
 }
