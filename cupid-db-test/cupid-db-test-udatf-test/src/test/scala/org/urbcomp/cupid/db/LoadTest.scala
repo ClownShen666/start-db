76be87436526d921dfa797e975a4c71abb9a7398
zcc0de <zechao.chen@kangry.net>
2023-05-27 19:42:09 +0800

    Add test

--- cupid-db-test/cupid-db-test-udatf-test/src/test/scala/org/urbcomp/cupid/db/LoadTest.scala
+++ cupid-db-test/cupid-db-test-udatf-test/src/test/scala/org/urbcomp/cupid/db/LoadTest.scala
@@ -16,8 +16,27 @@
  */
 package org.urbcomp.cupid.db
 
+import org.junit.Assert.assertNotNull
+import org.urbcomp.cupid.db.tools.CitibikeDataUtils
+
+import java.util.UUID
+
 class LoadTest extends AbstractCalciteSparkFunctionTest {
   test("test load") {
-    // TODO: Write a proper test
+    val path = CitibikeDataUtils.getProjectRoot + "/cupid-db-test/cupid-db-test-geomesa-geotools/src/main/resources/" + "202204-citibike-tripdata_clip_slice.csv"
+    val randTableName = s"Table_${UUID.randomUUID().toString.replace("-", "_")}"
+    val loadSql =
+      s"""LOAD CSV INPATH $path TO $randTableName (
+         |  idx idx, ride_id ride_id,
+         |  rideable_type rideable_type,
+         |  start_point st_makePoint(start_lat, start_lng))
+         |  WITH HEADER""".stripMargin
+    val querySql = s"select count(1) from $randTableName"
+
+    val stmt = connect.createStatement()
+    stmt.execute(loadSql)
+
+    val resultSet = stmt.executeQuery(querySql)
+    assertNotNull(resultSet)
   }
 }
