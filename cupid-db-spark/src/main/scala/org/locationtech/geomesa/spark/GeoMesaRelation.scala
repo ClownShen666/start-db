ac62765ca1791b282f512822f3443022966ef259
jack <1071699510@qq.com>
2023-07-11 20:04:20 +0800

    del udf

--- cupid-db-spark/src/main/scala/org/locationtech/geomesa/spark/GeoMesaRelation.scala
+++ cupid-db-spark/src/main/scala/org/locationtech/geomesa/spark/GeoMesaRelation.scala
@@ -174,7 +174,7 @@ case class GeoMesaRelation(
             SelfClosingIterator(engine.getFeatureReader(query, Transaction.AUTO_COMMIT))
         }
     }
-
+    println("I have get the place of buildscan  ")
     val extractors = SparkUtils.getExtractors(requiredColumns, schema)
     result.map(SparkUtils.sf2row(schema, _, extractors))
   }
