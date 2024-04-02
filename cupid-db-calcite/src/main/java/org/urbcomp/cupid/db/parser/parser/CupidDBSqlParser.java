66510739537e612a01b3a5d711d14422732bae80
FrancoiseSagan <2537953899@qq.com>
2024-04-02 21:43:42 +0800

    fix

--- cupid-db-calcite/src/main/java/org/urbcomp/cupid/db/parser/parser/CupidDBSqlParser.java
+++ cupid-db-calcite/src/main/java/org/urbcomp/cupid/db/parser/parser/CupidDBSqlParser.java
@@ -9057,8 +9057,8 @@ public class CupidDBSqlParser extends Parser {
                     && !_input.LT(1).getText().equalsIgnoreCase("LIMIT")
                     && !_input.LT(1).getText().equalsIgnoreCase("WITH")))
                     throw new FailedPredicateException(
-                        this,
-                        "!_input.LT(1).getText().equalsIgnoreCase(\"EXEC\") &&\r\n        !_input.LT(1).getText().equalsIgnoreCase(\"EXECUTE\") &&\r\n        !_input.LT(1).getText().equalsIgnoreCase(\"INNER\") &&\r\n        !_input.LT(1).getText().equalsIgnoreCase(\"LEFT\") &&\r\n        !_input.LT(1).getText().equalsIgnoreCase(\"GROUP\") &&\r\n        !_input.LT(1).getText().equalsIgnoreCase(\"ORDER\") &&\r\n        !_input.LT(1).getText().equalsIgnoreCase(\"LIMIT\") &&\r\n        !_input.LT(1).getText().equalsIgnoreCase(\"WITH\")"
+                            this,
+                            "!_input.LT(1).getText().equalsIgnoreCase(\"EXEC\") &&\r\n        !_input.LT(1).getText().equalsIgnoreCase(\"EXECUTE\") &&\r\n        !_input.LT(1).getText().equalsIgnoreCase(\"INNER\") &&\r\n        !_input.LT(1).getText().equalsIgnoreCase(\"LEFT\") &&\r\n        !_input.LT(1).getText().equalsIgnoreCase(\"GROUP\") &&\r\n        !_input.LT(1).getText().equalsIgnoreCase(\"ORDER\") &&\r\n        !_input.LT(1).getText().equalsIgnoreCase(\"LIMIT\") &&\r\n        !_input.LT(1).getText().equalsIgnoreCase(\"WITH\")"
                     );
                 setState(1121);
                 _errHandler.sync(this);
